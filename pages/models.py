63aac6e0ced108a5bfba902c4a3f769b0cb5efa4
FNaouchi <f.naoushi@hotmail.com>
2018-10-08 14:48:51 +0300

    task submission

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Page(models.Model):
+	title = models.CharField(max_length=200)
+	content = models.TextField()
+	last_update = models.DateTimeField(auto_now_add=True)
+	def __str__(self):
+		return self.title
+	def get_absolute_url(self):
+		urls = {
+        'ABSOLUTE_ROOT': request.build_absolute_uri('/')[:-1].strip("/"),
+        'ABSOLUTE_ROOT_URL': request.build_absolute_uri('/').strip("/"),
+    }
+
+    return urls
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+import .models import Page
 # Create your views here.
+
+def page_list(request):
+	context = {
+		"pages" : Page.object.all()
+		}
+	return render(request, 'list.html', context)
+
+
+def page_detail(request, page_id):
+	context = {
+		"page" : Page.object.get(id = page_id)
+		}
+	return render(request, 'detail.html', context)
