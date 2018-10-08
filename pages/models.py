92930f8aa6a0482bac99d14a3a8cb7aeabdf10ff
FNaouchi <f.naoushi@hotmail.com>
2018-10-08 15:43:11 +0300

    task submission

--- pages/models.py
+++ pages/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 class Page(models.Model):
 	title = models.CharField(max_length=200)
 	content = models.TextField()
-	last_update = models.DateTimeField(auto_now_add=True)
+	last_update = models.DateTimeField(auto_now=True)
 	def __str__(self):
 		return self.title
 	def get_absolute_url(self):
